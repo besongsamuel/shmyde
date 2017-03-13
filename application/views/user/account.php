b44c3fbb17654daae412bbceb49dbb8370126753
besongsamuel <besongsamuel@yahoo.com>
2017-03-13 10:10:24 -0400

    Create account.php
    Created first draft of account page.

---
+++ application/views/user/account.php
@@ -0,0 +1,13 @@
+
+
+<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+<div class="container">
+    <div class="shmyde-profile">
+        <img align="left" class="shmyde-image-lg" src="http://lorempixel.com/850/280/nightlife/5/" alt="Profile image example"/>
+        <img align="left" class="shmyde-image-profile thumbnail" src="http://lorempixel.com/180/180/people/9/" alt="Profile image example"/>
+        <div class="shmyde-profile-text">
+            <h1>{{userObject.user.last_name + ", " + userObject.user.first_name}}</h1>
+            <p>userObject.user.email.</p>
+        </div>
+    </div>
+</div> <!-- /container -->  

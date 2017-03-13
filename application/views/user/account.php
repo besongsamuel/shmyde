ccdbfe8663af23b9618b6fc1dff9febe310dcd6e
besongsamuel <besongsamuel@yahoo.com>
2017-03-13 10:44:38 -0400

    Update account.php

--- application/views/user/account.php
+++ application/views/user/account.php
@@ -1,5 +1,5 @@
 
-
+<!-- Profile Header -->
 <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
 <div class="container">
     <div class="shmyde-profile">
@@ -10,4 +10,131 @@
             <p>userObject.user.email.</p>
         </div>
     </div>
-</div> <!-- /container -->  
+</div> 
+
+<!-- Edit User Details Section -->
+  <div class="row">
+    <div class="col-md-4 col-md-offset-4">
+      <form class="form-horizontal" role="form">
+        <fieldset>
+
+          <!-- Form Name -->
+          <legend>User Details</legend>
+            
+            <!-- Last name and First name input-->
+            <div class="form-group">
+                <label class="col-sm-2 control-label" for="last_name">Last Name</label>
+                <div class="col-sm-4">
+                    <input type="text" id="last_name" name="last_name" placeholder="Last Name" class="form-control" ng-model="userObject.user.last_name">
+                </div>
+
+                <label class="col-sm-2 control-label" for="first_name">First Name</label>
+                <div class="col-sm-4">
+                    <input type="text" id="first_name" name="first_name" placeholder="First Name" class="form-control" ng-model="userObject.user.first_name">
+                </div>
+            </div>
+            
+          <!-- Gender input-->
+          <div class="form-group">
+                <label class="col-sm-2 control-label" for="gender">Gender</label>
+                <div class="col-sm-10">
+                    <select name="gender" id="gender" ng-model="userObject.user.gender" class="form-control">
+                        <option value='none'>-- Select Gender --</option>
+                        <option value='male'>Male</option>
+                        <option value='female'>Female</option>
+                        <option value='other'>Other</option>
+                    </select>
+                </div>
+          </div>
+            
+          <!-- DoB input-->
+          <div class="form-group">
+                <label class="col-sm-2 control-label" for="dob">Line 1</label>
+                <div class="col-sm-10">
+                    <input type="date" name="dob" id="dob" ng-model="userObject.user.dob" class="form-control">
+                </div>
+          </div>   
+
+          <div class="form-group">
+            <div class="col-sm-offset-2 col-sm-10">
+              <div class="pull-right">
+                <button type="submit" class="btn btn-default">Cancel</button>
+                <button type="submit" class="btn btn-primary">Save</button>
+              </div>
+            </div>
+          </div>
+
+        </fieldset>
+      </form>
+    </div><!-- /.col-lg-12 -->
+</div><!-- /.row -->
+
+<!-- Edit Address Section -->
+  <div class="row">
+    <div class="col-md-4 col-md-offset-4">
+      <form class="form-horizontal" role="form">
+        <fieldset>
+
+          <!-- Form Name -->
+          <legend>Address Details</legend>
+
+          <!-- Text input-->
+          <div class="form-group">
+            <label class="col-sm-2 control-label" for="textinput">Line 1</label>
+            <div class="col-sm-10">
+              <input type="text" placeholder="Address Line 1" class="form-control">
+            </div>
+          </div>
+
+          <!-- Text input-->
+          <div class="form-group">
+            <label class="col-sm-2 control-label" for="textinput">Line 2</label>
+            <div class="col-sm-10">
+              <input type="text" placeholder="Address Line 2" class="form-control">
+            </div>
+          </div>
+
+          <!-- Text input-->
+          <div class="form-group">
+            <label class="col-sm-2 control-label" for="textinput">City</label>
+            <div class="col-sm-10">
+              <input type="text" placeholder="City" class="form-control">
+            </div>
+          </div>
+
+          <!-- Text input-->
+          <div class="form-group">
+            <label class="col-sm-2 control-label" for="textinput">State</label>
+            <div class="col-sm-4">
+              <input type="text" placeholder="State" class="form-control">
+            </div>
+
+            <label class="col-sm-2 control-label" for="textinput">Postcode</label>
+            <div class="col-sm-4">
+              <input type="text" placeholder="Post Code" class="form-control">
+            </div>
+          </div>
+
+
+
+          <!-- Text input-->
+          <div class="form-group">
+            <label class="col-sm-2 control-label" for="textinput">Country</label>
+            <div class="col-sm-10">
+              <input type="text" placeholder="Country" class="form-control">
+            </div>
+          </div>
+
+          <div class="form-group">
+            <div class="col-sm-offset-2 col-sm-10">
+              <div class="pull-right">
+                <button type="submit" class="btn btn-default">Cancel</button>
+                <button type="submit" class="btn btn-primary">Save</button>
+              </div>
+            </div>
+          </div>
+
+        </fieldset>
+      </form>
+    </div><!-- /.col-lg-12 -->
+</div><!-- /.row -->

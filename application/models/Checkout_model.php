eae1287e0e118e232f6bfabcc2affa66e5678e11
besongsamuel <besongsamuel@yahoo.com>
2017-04-17 12:52:05 -0400

    Update Checkout_model.php
    Add order_id to checkout model

--- application/models/Checkout_model.php
+++ application/models/Checkout_model.php
@@ -24,37 +24,48 @@ class Checkout_model extends CI_Model {
 	/**
          * 
          * @param type $user_id
+	 * @param type $order_id
          * @param type $quantity
          * @param type $price
          * @param type $type the type of the design
          * @param type $design_data
          * @return boolean
          */
-	public function checkout($user_id, $quantity, $price, $type, $design_data, $frontBase64Image, $backBase64Image) 
+	public function checkout($user_id, $order_id, $quantity, $price, $type, $design_data, $frontBase64Image, $backBase64Image) 
         {
 		
-            $data = array(
-                    'user_id'           => $user_id,
-                    'price'             => $price,
-                    'quantity'          => $quantity,
-                    'type'              => $type,
-                    'status'            => 0,
-                    'date_created'      => date('Y-m-j H:i:s'),
-                    'date_modified'     => date('Y-m-j H:i:s'),
-                    'user_design'       => $design_data,
-            );
+	    $data = array(
+		    'user_id'           => $user_id,
+		    'price'             => $price,
+		    'quantity'          => $quantity,
+		    'type'              => $type,
+		    'status'            => 0,
+		    'date_created'      => date('Y-m-j H:i:s'),
+		    'date_modified'     => date('Y-m-j H:i:s'),
+		    'user_design'       => $design_data,
+	    );
+		
+		$insert_id = -1;
 
-            $result =  $this->db->insert('shmyde_orders', $data);
+		if($order_id == -1)
+		{
+			$result =  $this->db->insert('shmyde_orders', $data);
+			$insert_id = $this->db->insert_id();
+		}
+		else
+		{
+			$insert_id = $order_id;
+			$this->db->where('id', $insert_id);
+			$this->db->update('shmyde_orders', $data);
+		}
             
-            $insert_id = $this->db->insert_id();
-            
-            $decoded_frontBase64Image = base64_decode(preg_replace('#^data:image/\w+;base64,#i', '', $frontBase64Image));
-            $decoded_backBase64Image = base64_decode(preg_replace('#^data:image/\w+;base64,#i', '', $backBase64Image));
-            
-            file_put_contents(ASSETS_DIR_PATH.'/images/orders/order_'.$insert_id.'_'.$user_id.'_front.png', $decoded_frontBase64Image);
-            file_put_contents(ASSETS_DIR_PATH.'/images/orders/order_'.$insert_id.'_'.$user_id.'_back.png', $decoded_backBase64Image);
-            
-            return $result;
+	    	$decoded_frontBase64Image = base64_decode(preg_replace('#^data:image/\w+;base64,#i', '', $frontBase64Image));
+	    	$decoded_backBase64Image = base64_decode(preg_replace('#^data:image/\w+;base64,#i', '', $backBase64Image));
+
+	    	file_put_contents(ASSETS_DIR_PATH.'/images/orders/order_'.$insert_id.'_'.$user_id.'_front.png', $decoded_frontBase64Image);
+	    	file_put_contents(ASSETS_DIR_PATH.'/images/orders/order_'.$insert_id.'_'.$user_id.'_back.png', $decoded_backBase64Image);
+
+	    	return $result;
 		
 	}
 	

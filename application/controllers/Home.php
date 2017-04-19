558f76fedc7139d518776ef3fa6adb9f851528a3
besongsamuel <besongsamuel@yahoo.com>
2017-04-19 13:11:53 -0400

    Update Home.php
    Added controller code to send users comment to Shmyde via contact page.

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -18,6 +18,53 @@ class Home extends CI_Controller {
             $this->data['products'] = json_encode($this->home_model->get_products());
                         
             $this->template->load('shmyde', 'home/index', $this->data);
-        }	
+        }
+        
+        /// <summary>
+        /// This method is called when the user clicks on 
+        /// The send in the contact us form in the client
+        /// </summary>
+        public function ContactUs()
+        {
+                $contactName = $this->input->post('contactName');
+                $contactEmail = $this->input->post('contactEmail');
+                $contactComment = $this->input->post('contactComment');
+                set_error_handler(function(){ });
+                $this->send_contactus_mail($contactName, $contactEmail, $contactComment);
+                restore_error_handler();
+        }
+        
+        /// <summary>
+        /// Sends an email to the shmyde team containing the user's comment
+        /// </summary>
+        /// <param name="$contactName">The name of the contact</param>
+        /// <param name="$contactEmail">The email of the contact</param>
+        /// <param name="$contactComment">The comment of the contact</param>
+        private function send_contactus_mail($contactName, $contactEmail, $contactComment)
+        {
+                $subject = "Comment From Customer - ";
+                $headers = "From: ".$contactEmail." \r\n";
+                $headers .= "Reply-To: ".$contactEmail." \r\n";
+                $headers .= "MIME-Version: 1.0\r\n";
+                $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+                $message = '<html><body>';
+                $message.='<div style="width:550px; background-color:#CC6600; padding:15px; font-weight:bold;">';
+                $message.='Dear Shmyde Team, ';
+                $message.='</div>';
+                $message.='
+                <div style="font-family: Arial;">
+                <p>Find below my comments </p>
+                <p></p>
+                <p></p>
+                <p align="center">'.$contactComment.'</p>
+                <p></p>
+                <br/>
+                <p>Best Regards</p>
+                <p>'.$contactName.'</p>
+                ';
+                $message.='</div>';
+                $message.='</body></html>';
+                mail(SHMYDE_CONTACT,$subject,$message,$headers);
+        }
 }
 

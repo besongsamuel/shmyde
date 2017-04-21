da038c4b5dd8379c515f1f5be7b8cbf1f8cbc5d8
besongsamuel <besongsamuel@yahoo.com>
2017-04-21 16:03:53 -0400

    Update User_model.php
    -- Add method to validate if user password is correct
    --  Add DB methods to update user details and user password

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -77,6 +77,26 @@ class User_model extends CI_Model {
 	}
 	
 	/**
+	 * resolve_user_login function using the user id.
+	 * 
+	 * @access public
+	 * @param mixed $email
+	 * @param mixed $user_id
+	 * @return bool true on success, false on failure
+	 */
+	public function valid_password($user_id, $password) 
+        {
+		
+            $this->db->select('password');
+            $this->db->from('users');
+            $this->db->where('id', $user_id);
+            $hash = $this->db->get()->row('password');
+
+            return $this->verify_password_hash($password, $hash);
+		
+	}
+	
+	/**
 	 * get_user_id_from_username function.
 	 * 
 	 * @access public
@@ -340,4 +360,20 @@ class User_model extends CI_Model {
 
             return null;
         }
+	
+	public function update_user_details($user_id, $data) 
+        {
+            	$this->db->where("user_id", $user_id);
+		$this->db->update(USER_DATA_TABLE, $data);
+        }
+	
+	public function update_user_password($user_id, $new_password)
+	{
+		$data = array("password" => $new_password);
+		
+		$this->db->where("id", $user_id);
+		$this->db->update(USER_TABLE, $data);
+	}
+	
+	
 }

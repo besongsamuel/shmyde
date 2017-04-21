c6b0d1e8b80b4a395267d6a1418bd1b94d2cd922
besongsamuel <besongsamuel@yahoo.com>
2017-04-21 15:03:35 -0400

    Update account.php

--- application/views/user/account.php
+++ application/views/user/account.php
@@ -12,6 +12,15 @@
         userScope.$apply(function()
         {
             userScope.user_orders = JSON.parse('<?php echo $shmyde_orders; ?>');
+            userScope.first_name = userScope.userObject.user.first_name;
+            userScope.last_name = userScope.userObject.user.last_name;
+            userScope.gender = userScope.userObject.user.gender;
+            userScope.dob = userScope.userObject.user.dob;
+            userScope.address_line_1 = userScope.userObject.user.address_line_1;
+            userScope.address_line_2 = userScope.userObject.user.address_line_2;
+            userScope.country = userScope.userObject.user.country;
+            userScope.city = userScope.userObject.user.city;
+            userScope.loading = false;
         });
     });
 </script>
@@ -30,7 +39,7 @@
 <!-- Edit User Details Section -->
   <div class="row">
     <div class="col-md-8 col-md-offset-2">
-      <form class="form-horizontal" role="form">
+      <form class="form-horizontal" role="form" novalidate ng-submit="saveUserDetails()">
         <fieldset>
 
           <!-- Form Name -->
@@ -40,12 +49,12 @@
             <div class="form-group">
                 <label class="col-sm-2 control-label" for="last_name">Last Name</label>
                 <div class="col-sm-4">
-                    <input type="text" id="last_name" name="last_name" placeholder="Last Name" class="form-control" ng-model="userObject.user.last_name">
+                    <input type="text" id="last_name" name="last_name" placeholder="Last Name" class="form-control" ng-model="last_name">
                 </div>
 
                 <label class="col-sm-2 control-label" for="first_name">First Name</label>
                 <div class="col-sm-4">
-                    <input type="text" id="first_name" name="first_name" placeholder="First Name" class="form-control" ng-model="userObject.user.first_name">
+                    <input type="text" id="first_name" name="first_name" placeholder="First Name" class="form-control" ng-model="first_name">
                 </div>
             </div>
             
@@ -53,7 +62,7 @@
               <div class="form-group">
                 <label class="col-sm-2 control-label" for="gender">Gender</label>
                 <div class="col-sm-10">
-                    <select name="gender" id="gender" ng-model="userObject.user.gender" class="form-control">
+                    <select name="gender" id="gender" ng-model="gender" class="form-control">
                         <option value='none'>-- Select Gender --</option>
                         <option value='male'>Male</option>
                         <option value='female'>Female</option>
@@ -66,14 +75,14 @@
           <div class="form-group">
                 <label class="col-sm-2 control-label" for="dob">Date of Birth</label>
                 <div class="col-sm-10">
-                    <input type="date" name="dob" id="dob" ng-model="userObject.user.dob" class="form-control">
+                    <input type="date" name="dob" id="dob" ng-model="dob" class="form-control">
                 </div>
           </div>   
 
           <div class="form-group">
             <div class="col-sm-offset-2 col-sm-10">
               <div class="pull-right">
-                <button type="submit" class="btn btn-default">Cancel</button>
+                <button class="btn btn-default" ng-click="cancelUserDetails()">Cancel</button>
                 <button type="submit" class="btn btn-primary">Save</button>
               </div>
             </div>
@@ -84,10 +93,59 @@
     </div><!-- /.col-lg-12 -->
 </div><!-- /.row -->
 
+<!-- Change Password -->
+  <div class="row">
+    <div class="col-md-8 col-md-offset-2">
+      <form name="changePasswordForm" class="form-horizontal" role="form" novalidate ng-submit="changeUserPassword()">
+        <fieldset>
+
+          <!-- Form Name -->
+          <legend>Change Password</legend>
+
+          <!-- Old Password-->
+          <div class="form-group">
+            <label class="col-sm-2 control-label" for="textinput">Enter Old Password</label>
+            <div class="col-sm-10">
+              <input type="password" placeholder="Old Password"  ng-model="old-password" name="old-password" id='old-password' class="form-control">
+            </div>
+          </div>
+          
+          <!-- New Password -->
+          <div class="form-group">
+            <label class="col-sm-2 control-label" for="textinput">New Password</label>
+            <div class="col-sm-10">
+              <input type="password" placeholder="New Password" ng-model="new-password" name="new-password" id='new-password' class="form-control">
+            </div>
+          </div> 
+            
+          <!-- Confirm New Password -->
+          <div class="form-group">
+            <label class="col-sm-2 control-label" for="textinput">Confirm New Password</label>
+            <div class="col-sm-10">
+              <input type="password" placeholder="Confirm New Password" ng-model="confirm-new-password" name="confirm-new-password" id='confirm-new-password' class="form-control">
+            </div>
+          </div>   
+
+          <div class="form-group">
+            <div class="col-sm-offset-2 col-sm-10">
+              <div class="pull-right">
+                <button class="btn btn-default" ng-click="cancelUserPassword()">Cancel</button>
+                <button type="submit" class="btn btn-primary">Save</button>
+              </div>
+            </div>
+          </div>
+
+        </fieldset>
+      </form>
+    </div><!-- /.col-lg-12 -->
+</div><!-- /.row -->
+    
+    
+
 <!-- Edit Address Section -->
   <div class="row">
     <div class="col-md-8 col-md-offset-2">
-      <form class="form-horizontal" role="form">
+      <form class="form-horizontal" role="form" novalidate ng-submit="changeUserAddress()">
         <fieldset>
 
           <!-- Form Name -->
@@ -134,14 +192,16 @@
           <div class="form-group">
             <label class="col-sm-2 control-label" for="textinput">Country</label>
             <div class="col-sm-10">
-              <input type="text" placeholder="Country" class="form-control">
+                <select  class="form-control"  ng-model="country" name="country" pvp-country-picker>
+
+                </select>
             </div>
           </div>
 
           <div class="form-group">
             <div class="col-sm-offset-2 col-sm-10">
               <div class="pull-right">
-                <button type="submit" class="btn btn-default">Cancel</button>
+                <button class="btn btn-default" ng-click="cancelUserAddress()">Cancel</button>
                 <button type="submit" class="btn btn-primary">Save</button>
               </div>
             </div>
@@ -152,7 +212,7 @@
     </div><!-- /.col-lg-12 -->
 </div><!-- /.row -->
 
-<!-- Edit User Details Section -->
+<!-- User Orders -->
 <div class="row" id="orders">
     <div class="col-md-8 col-md-offset-2">
         <form class="form-horizontal" role="form">
